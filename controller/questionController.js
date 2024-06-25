b6c3740a8bb282cb8dc5a9cfa60488937c6145b3
Samtmeg <160788397+Samtmeg@users.noreply.github.com>
2024-06-24 20:06:33 -0400

    updated question controller to use mysql

--- controller/questionController.js
+++ controller/questionController.js
@@ -5,62 +5,62 @@ import { StatusCodes } from "http-status-codes"
 import { db } from "../db/firebase.js"
 import { QuerySnapshot } from "firebase/firestore"
 
-async function allQuestions (req, res) {
-    
-    try {
-        // const [questions] = await dbconnection.query("SELECT userid, questionid, title, description FROM questions")
-
-        const snapshot = await db.collection('questions').get()
-        const questions = []
-        snapshot.docs.forEach(doc => {questions.push(doc.data())})
-
-        return res.status(StatusCodes.OK).json(questions)
-    } catch (err) {
-        console.log(err.message)
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
-    }
-}
-
-
-async function submitQuestion (req, res) {
-    const {title, description, tag} = req.body;
-    const userid = req.user.userid
-    try {
-
-        var newqnid = generateUniqueId();
-        const qnid = await dbconnection.query("SELECT title FROM questions where questionid = ?", newqnid)
-        while (newqnid === qnid){
-            newqnid = generateUniqueId();
-        }
-
-        
-        //add user to firestore db
-        await db.collection('questions').add({
-            userid: userid,
-            title: title,
-            description: description,
-            tag: tag,
-        })
-
-        return res.status(StatusCodes.CREATED).json({msg:"question added successfully"})
-        
-    } catch (err) {
-        console.log(err.message)
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
-    }
-}
-
 // async function allQuestions (req, res) {
     
 //     try {
 //         // const [questions] = await dbconnection.query("SELECT userid, questionid, title, description FROM questions")
-//         const [questions] = await dbconnection.query("SELECT t1.*, t2.username FROM questions t1 LEFT JOIN users t2 ON t1.userid = t2.userid")
+
+//         const snapshot = await db.collection('questions').get()
+//         const questions = []
+//         snapshot.docs.forEach(doc => {questions.push(doc.data())})
+
 //         return res.status(StatusCodes.OK).json(questions)
 //     } catch (err) {
 //         console.log(err.message)
 //         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
 //     }
 // }
+
+
+// async function submitQuestion (req, res) {
+//     const {title, description, tag} = req.body;
+//     const userid = req.user.userid
+//     try {
+
+//         var newqnid = generateUniqueId();
+//         const qnid = await dbconnection.query("SELECT title FROM questions where questionid = ?", newqnid)
+//         while (newqnid === qnid){
+//             newqnid = generateUniqueId();
+//         }
+
+        
+//         //add user to firestore db
+//         await db.collection('questions').add({
+//             userid: userid,
+//             title: title,
+//             description: description,
+//             tag: tag,
+//         })
+
+//         return res.status(StatusCodes.CREATED).json({msg:"question added successfully"})
+        
+//     } catch (err) {
+//         console.log(err.message)
+//         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
+//     }
+// }
+
+async function allQuestions (req, res) {
+    
+    try {
+        // const [questions] = await dbconnection.query("SELECT userid, questionid, title, description FROM questions")
+        const [questions] = await dbconnection.query("SELECT t1.*, t2.username FROM questions t1 LEFT JOIN users t2 ON t1.userid = t2.userid")
+        return res.status(StatusCodes.OK).json(questions)
+    } catch (err) {
+        console.log(err.message)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
+    }
+}
 
 async function goToQuestion (req, res) {
     const {questionid} = req.query
@@ -74,25 +74,26 @@ async function goToQuestion (req, res) {
     }
 }
 
-// async function submitQuestion (req, res) {
-//     const {title, description, tag} = req.body;
-//     const userid = req.user.userid
-//     try {
-        
-//         var newqnid = generateUniqueId();
-//         const qnid = await dbconnection.query("SELECT title FROM questions where questionid = ?", newqnid)
-//         while (newqnid === qnid){
-//             newqnid = generateUniqueId();
-//         }
+async function submitQuestion (req, res) {
 
-//         await dbconnection.query("INSERT INTO questions (questionid, userid, title, description, tag) VALUES (?,?,?,?,?)", [newqnid, userid, title, description, tag]) 
-
-//         return res.status(StatusCodes.CREATED).json({msg:"question added successfully"})
+    const {title, description, tag} = req.body;
+    const userid = req.user.userid
+    try {
         
-//     } catch (err) {
-//         console.log(err.message)
-//         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
-//     }
-// }
+        var newqnid = generateUniqueId();
+        const qnid = await dbconnection.query("SELECT title FROM questions where questionid = ?", newqnid)
+        while (newqnid === qnid){
+            newqnid = generateUniqueId();
+        }
+
+        await dbconnection.query("INSERT INTO questions (questionid, userid, title, description, tag) VALUES (?,?,?,?,?)", [newqnid, userid, title, description, tag]) 
+
+        return res.status(StatusCodes.CREATED).json({msg:"question added successfully"})
+        
+    } catch (err) {
+        console.log(err.message)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong. please try later"})
+    }
+}
 
 export {allQuestions, goToQuestion, submitQuestion};
